156e77b0bb4ce4f22b144ffe6ff6634166d683da
ArabicCourse <arabiccourse@yahoo.com>
2024-07-31 11:15:10 +0300

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/register', {
+            const response = await axios.post('https://course-backend-ajbr.onrender.com/api/register', {
                 name,
                 email,
                 password,

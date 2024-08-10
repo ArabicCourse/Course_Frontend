1c00b9ecad544d9b70785881e38154a94c076aff
ArabicCourse <arabiccourse@yahoo.com>
2024-08-10 22:26:59 +0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,21 @@
-// src/Login.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './login.css';  // Create a CSS file for styling if needed
+import './login.css';  // Ensure this file contains the necessary styles
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);  // New state for loading
 
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);  // Start loading
+        setMessage('');    // Clear any previous messages
+
         try {
             const response = await axios.post('https://course-backend-ajbr.onrender.com/api/login', { email, password });
             if (response.data.auth !== undefined) {
@@ -29,6 +32,8 @@ const Login = () => {
                 console.error('There was an error logging in!', error);
                 setMessage('Error logging in');
             }
+        } finally {
+            setLoading(false);  // Stop loading
         }
     };
 
@@ -58,7 +63,9 @@ const Login = () => {
                             required
                         />
                     </div>
-                    <button type="submit">Login</button>
+                    <button type="submit" disabled={loading}>
+                        {loading ? 'Loading...' : 'Login'}
+                    </button>
                     <button type="button" onClick={() => navigate('/register')}>Go to Register</button>
                 </form>
                 {message && <p>{message}</p>}

46332c9fb44c937399689f58daffc445f44c77ca
ArabicCourse <arabiccourse@yahoo.com>
2024-09-08 21:24:23 +0300

    Update Payment.js

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -25,7 +25,7 @@ const Payment = () => {
             formData.append('phone', phone);
             formData.append('file', file);
     
-            fetch('https://course-backend-ajbr.onrender.com/api/payment', {
+            fetch('http://localhost:5000/api/payment', {
                 method: 'POST',
                 body: formData
             }).then(response => response.json())
